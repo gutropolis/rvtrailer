3053b0cc6b817eaa531954a27051dfdf436e6ff8
gutropolis <gutropolis@gmail.com>
2018-01-29 12:32:19 +0530

    add renter image

--- src/app/shared/edit-about-user/edit-about-user.component.ts
+++ src/app/shared/edit-about-user/edit-about-user.component.ts
@@ -8,7 +8,9 @@ import { FileUploader } from 'ng2-file-upload';
 
 // const URL = '/api/';
 //const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
-const URL = 'http://localhost:3001/upload';
+//const URL = 'http://localhost:3001/upload';
+
+const URL='http://104.236.9.249:3001/upload';
 
 // const URL: string = 'http://162.243.111.79:3001';
 
