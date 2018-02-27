1ea3fb8941b74adb370d6f94fa350bae84c7dc41
gutropolis <gutropolis@gmail.com>
2018-02-27 19:36:16 +0530

    changes for server

--- src/app/shared/owner-edit-listtrailer/owner-edit-listtrailer.component.ts
+++ src/app/shared/owner-edit-listtrailer/owner-edit-listtrailer.component.ts
@@ -6,9 +6,9 @@ import { FileUploader } from 'ng2-file-upload';
 import { formControlBinding } from '@angular/forms/src/directives/ng_model';
 // const URL = '/api/';
 //const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
- const URL = 'http://localhost:3001/upload';
+ //const URL = 'http://localhost:3001/upload';
  //const URL = 'http://162.243.111.79:3001/upload';
-// const URL='http://104.236.9.249:3001/upload';
+ const URL='http://104.236.9.249:3001/upload';
 @Component({
   selector: 'rv-owner-edit-listtrailer',
   templateUrl: './owner-edit-listtrailer.component.html',
