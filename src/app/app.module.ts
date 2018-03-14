c5cb793fd48eace3ca6efa9140f1342eb6afce00
gutropolis <gutropolis@gmail.com>
2018-03-14 16:38:28 +0530

    add by a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -124,7 +124,7 @@ import { OrderbyPipe } from './orderby.pipe';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import {NgxPaginationModule} from 'ngx-pagination';
 import { AgmCoreModule } from '@agm/core';
-import { FusionChartsModule } from 'angular4-fusioncharts';
+//import { FusionChartsModule } from 'angular4-fusioncharts';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBkxVWfEOYenaJYNgb-12NV5Arp0tOGa8k",
